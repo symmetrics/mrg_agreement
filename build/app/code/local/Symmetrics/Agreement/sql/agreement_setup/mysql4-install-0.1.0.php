177a01b73e834292ffbddd375e73f48f86255054
Eugen Gitin <devnull@localhost>
2009-05-21 23:10:37 +0200

    #1647 Fixed Seiten AGB und Widerruf werden jetzt sofort nach Install aktiviert

--- build/app/code/local/Symmetrics/Agreement/sql/agreement_setup/mysql4-install-0.1.0.php
+++ build/app/code/local/Symmetrics/Agreement/sql/agreement_setup/mysql4-install-0.1.0.php
@@ -34,11 +34,24 @@ INSERT INTO `cms_page` (`title`, `root_template`, `meta_keywords`, `meta_descrip
 ('AGB', 'one_column', '', '', 'agb', '{{block type="cms/block" block_id="sym_agb"}}', '$dateTime', '$dateTime', 1, 0, '', '', NULL, NULL);
 EOF;
 $installer->run($query);
-	
+    
 $newEntityId = $installer->getConnection()->lastInsertId();
 $query = <<< EOF
 INSERT INTO `cms_page_store` (`page_id`, `store_id`) VALUES ('$newEntityId', '0');
 EOF;
+$installer->run($query);
+
+$query = <<< EOF
+INSERT INTO `cms_page` (`title`, `root_template`, `meta_keywords`, `meta_description`, `identifier`, `content`, `creation_time`, `update_time`, `is_active`, `sort_order`, `layout_update_xml`, `custom_theme`, `custom_theme_from`, `custom_theme_to`) VALUES
+('Widerrufsbelehrung', 'one_column', '', '', 'agb', '{{block type="cms/block" block_id="sym_widerruf"}}', '$dateTime', '$dateTime', 1, 0, '', '', NULL, NULL);
+EOF;
+$installer->run($query);
+    
+$newEntityId = $installer->getConnection()->lastInsertId();
+$query = <<< EOF
+INSERT INTO `cms_page_store` (`page_id`, `store_id`) VALUES ('$newEntityId', '0');
+EOF;
+$installer->run($query);
 
 $germanConfig = Mage::getConfig()->getNode('modules/Symmetrics_ConfigGerman');
 
