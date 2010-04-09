aaecfc23334ae370f8e05ff711f957d46295d62d
Eric Reiche <er@symmetrics.de>
2010-04-09 11:48:02 +0200

    Email-Template-Filter durch CMS-Filter ersetzt

--- build/app/code/community/Symmetrics/Agreement/Model/Agreement.php
+++ build/app/code/community/Symmetrics/Agreement/Model/Agreement.php
@@ -44,7 +44,7 @@ class Symmetrics_Agreement_Model_Agreement extends Mage_Checkout_Model_Agreement
     public function getContent()
     {
         $content = $this->getData('content');
-        $processor = Mage::getModel('core/email_template_filter');
+        $processor = Mage::getModel('cms/template_filter');
         $html = $processor->filter($content);
         
         return $html;
